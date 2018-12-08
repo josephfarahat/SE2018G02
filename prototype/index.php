9a2fa000e9c4e3c3df424c83a67bb18691366244
josephfarahat <36537848+josephfarahat@users.noreply.github.com>
2018-12-08 13:14:56 +0200

    Add files via upload

---
+++ prototype/index.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+if(isset( $_SESSION['login'] ) && $_SESSION['login'] != 1)
+	header('Location: login.php');
+if(isset($_GET['logout']) && !empty($_GET['logout'] ) )
+{
+	session_destroy();
+	header('Location: login.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>PollMe</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">PollMe</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="?logout=true">Logout</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+  <div class="header">
+  <img src="logo.jpg" alt="logo" />
+  <h2><dt><br>HELLO!</br>Welcome to PollMe...</dt></h2>
+</div>
+  <h2><dt>you can ask your questions and receive answers from others instantly!</dt></h2>
+  <h2><dt>Don't hesitate to join our community ... We care about your opinion.</dt></h2>
+</div>
+
+</body>
+</html>
---
+++ prototype/login.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+
+if(isset($_SESSION['login']) )
+if($_SESSION['login'] === 1)
+	header('Location: index.php');
+
+if(isset($_POST['user']) && isset($_POST['pass'] ))
+if($_POST['user'] === 'admin' && $_POST['pass'] === 'admin' )
+{
+	$_SESSION['login'] = 1;
+	header('Location: index.php');
+	
+}
+else 
+{
+	
+	echo '<script language="javascript">';
+echo 'alert("Please Enter Correct User and pass")';
+echo '</script>';
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>PollMe</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Login Page</h2>
+  <form method="post">
+    <div class="form-group">
+      <label for="usr">User Name:</label>
+      <input type="text" class="form-control" name="user" id="usr">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" name="pass" id="pwd">
+    </div>
+	<div class="form-group">
+      
+      <input type="submit" class="form-control">
+    </div>
+  </form>
+</div>
+
+</body>
+</html>
