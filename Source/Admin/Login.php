7e37348a7e72911a135e286eef39a9ddaaf5ab26
polarag <polaragheb@gmail.com>
2019-01-27 22:18:39 +0200

    Update Login.php

--- Source/Admin/Login.php
+++ Source/Admin/Login.php
@@ -1,1 +1,20 @@
-
+<?ph
+$valid = ( 
+if(!empty($_POST["user"]) && !empty($_POST["pass"]) )
+  {
+  }
+?>
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Login Page
+      </title>
+    </head>
+  <body>
+    <form method="post">
+      <input name="user" type="text"/>
+      <input name="pass" type="password"/>
+      <input type="submit"/>
+      </form>
+    </body>
+  </html>
