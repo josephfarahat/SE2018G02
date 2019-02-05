b80533478f1e05954d6b66ae50d0634b4ea3c91a
josephfarahat <36537848+josephfarahat@users.noreply.github.com>
2019-02-05 21:17:23 +0200

    Add files via upload

---
+++ testing/view_inq.php
@@ -0,0 +1,26 @@
+//code is linked to table created in myphpadmin 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "poll";
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT ID, question FROM inquiry";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> id: ". $row["ID"]. " - Name: ". $row["question"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
