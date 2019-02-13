a12f71f76251173aa7eea043400e64818480d7f7
Hamed Abdelreheem Fathallah <Hamed_fathallah@yahoo.com>
2019-02-13 22:16:34 +0200

    Update answerpoll.php

--- Source/PollME/answerpoll.php
+++ Source/PollME/answerpoll.php
@@ -77,107 +77,56 @@ SET r1 = '$a', r2 = '$b', r3 = '$c', r4 = '$d', r5 = '$e', r6 = '$f',no='$no' WH
 $result3 = $conn->query($sql);
 $y++;
 }
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Thank You</title>
-    <style >
-        body {
-    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-    font-size: 1rem;
-    line-height: 1.5;
-    color: #373a3c;
-    background-color: #fff;
-    height: 100%;
-    margin: 0;
-}
-.text-xs-center {
-    text-align: center!important;
-}
-.display-3 {
-    font-size: 4.5rem;
-    font-weight: 300;
-        margin-top: 0;
-}
-.lead {
-    font-size: 1.25rem;
-    font-weight: 300;
-}
-.btn-group-sm>.btn, .btn-sm {
-    padding: .25rem .5rem;
-    font-size: .875rem;
-    border-radius: .2rem;
-}
-.btn-primary {
-    color: #fff;
-    background-color: #0275d8;
-    border-color: #0275d8;
-}
-.btn {
-    display: inline-block;
-    font-weight: 400;
-    line-height: 1.25;
-    text-align: center;
-    white-space: nowrap;
-    vertical-align: middle;
-    cursor: pointer;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    border: 1px solid transparent;
-    padding: .5rem 1rem;
-    font-size: 1rem;
-    border-radius: .25rem;
-}
-p {
-    margin-top: 0;
-    margin-bottom: 1rem;
-}a{
-    text-decoration: none !important;
-}
-.btn-primary.active.focus, .btn-primary.active:focus, .btn-primary.active:hover, .btn-primary:active.focus, .btn-primary:active:focus, .btn-primary:active:hover, .open>.btn-primary.dropdown-toggle.focus, .open>.btn-primary.dropdown-toggle:focus, .open>.btn-primary.dropdown-toggle:hover {
-    color: #fff;
-    background-color: #014682;
-    border-color: #01315a;
-}
-.btn-primary.active, .btn-primary:active, .open>.btn-primary.dropdown-toggle {
-    color: #fff;
-    background-color: #025aa5;
-    border-color: #01549b;
-    background-image: none;
-}
-.jumbotron {
-    -webkit-box-pack: center!important;
-    -ms-flex-pack: center!important;
-    justify-content: center!important;
-    display: -webkit-box!important;
-    display: -ms-flexbox!important;
-    display: flex!important;
-        -webkit-box-align: center!important;
-    -ms-flex-align: center!important;
-    align-items: center!important;
-        -webkit-box-orient: vertical!important;
-    -webkit-box-direction: normal!important;
-    -ms-flex-direction: column!important;
-    flex-direction: column!important;
-    height: 100%;
-}
-html {
-    height: 100%;
-}
-    </style>
-</head>
-<body>
-<div class="jumbotron text-xs-center">
-  <h1 class="display-3">Thank You!</h1>
 
- 
- 
-  <p class="lead">
-    <a class="btn btn-primary btn-sm" href="index.php" role="button">Continue to homepage</a>
-  </p>
-</div>
-</body>
-</html>
+
+echo '
+<html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<title>
+          Poll me 
+    
+    	</title>
+    	<link rel="stylesheet"
+    		       type = "text/css"
+    		       href="bootstrap.css"
+
+    		   />
+			   <style>
+			   .fnt {
+				     position: absolute;
+                     top: 200px;
+                     left: 130px;
+                     font-size: 50px;
+			   }
+			   </style>
+           <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+           <link rel="stylesheet"
+               type = "text/css"
+               href="index.css"
+               
+           /><ul class="topnav">
+  <li><a class="active" href="index.php">Home</a></li>
+  <li><a href="answer_question.php"> Take Poll</a></li>
+  <li><a href="feedback2.html">Give feedback</a></li>
+  
+  <li><a href="inquiry.html">Inquiry</a></li>
+   <li><a href="view_inq.php">View</a></li>
+  <li class="right"><a href="login.php">Are you Speaker?</a></li>
+   
+  </ul>
+  <a href="#" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-google"></a>
+<p class="bottomleft"> Copyright 2019, Software Engineering Course, POLL ME. </p>
+<p class="fnt"> Thank you for your answer !</p>'
+?>
