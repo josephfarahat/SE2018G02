20869ed7781ecdcc25a4d3545bc20c35310096f3
HMarsafy <36539806+HMarsafy@users.noreply.github.com>
2019-02-14 18:20:36 +0200

    Add files via upload

--- Source/PollME/view_feedback.php
+++ Source/PollME/view_feedback.php
@@ -2,17 +2,58 @@
 include ('security.php');
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-	<title>feedback result</title>
-</head>
-<body>
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
 
-$dbname = "poll";
+<html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<title>
+          View Feedback
+    
+    	</title>
+    	<link rel="stylesheet"
+    		       type = "text/css"
+    		       href="bootstrap.css"
+
+    		   />
+           <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+           <link rel="stylesheet"
+               type = "text/css"
+               href="index.css"
+               
+           />
+           
+	</head> 
+	<body>
+<ul class="topnav">
+  <li><a class="active" href="logged.php">Home</a></li>
+  <li><a href="Admin.php">Create Poll</a></li>
+  <li><a href="view.php">View Answers</a></li>
+  <li><a href="view_feedback.php">View Feedback</a></li>
+  <li><a href="view_inq.php">View Inquiry</a></li>
+   <li><a href="Attachmentupload.php">Upload files</a></li>
+  <li class="right"><a href="logout.php">Logout </a></li>
+  </ul>
+   
+<p class="bottomleft"> Copyright 2019, Software Engineering Course, POLL ME. </p>
+
+
+
+
+<?php
+$servername = "sql202.epizy.com";
+$username = "epiz_23357401";
+$password = "jpvgo0IE";
+$dbname = "epiz_23357401_Poll";
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -33,8 +74,14 @@ if ($result2->num_rows > 0) {
         $dislike = $row["dislike"];
     }
 } 
-echo "the number of likes is " .$like."<br>";
-echo "the number of dislikes is ". $dislike."<br>";
+echo "<h2 style=margin-left:10px; > The number of likes is " .$like."</h2>";
+echo "<h2 style=margin-left:10px;> The number of dislikes is ". $dislike."</h2>";
 ?>
+<br><br>
+<h4 style=margin-left:10px;>If you want to reset the numbers to zero please click on clear</h4>
+<form method="post" action="delete.php">
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="submit" id='delete' class="button button1" name="delete" value='clear'></input>
+</form>
 </body>
 </html>
