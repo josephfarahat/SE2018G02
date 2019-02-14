d17bc7a3db8f3671de5890e57248dda81afe1bc7
Hamed Abdelreheem Fathallah <Hamed_fathallah@yahoo.com>
2019-02-14 20:12:34 +0200

    Update logged.php

--- Source/PollME/logged.php
+++ Source/PollME/logged.php
@@ -1,20 +1,10 @@
-
+<?php  include ('security.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
       <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    	<title>
-          Poll me 
-    
-    	</title>
-    	<link rel="stylesheet"
-    		       type = "text/css"
-    		       href="bootstrap.css"
-
-    		   />
-           <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <!-- jQuery library -->
@@ -27,24 +17,57 @@
                href="index.css"
                
            />
+
+
+    	<title>
+          Poll me 
+    
+    	</title>
+    	<link rel="stylesheet"
+    		       type = "text/css"
+    		       href="bootstrap.css"
+
+    		   />
+           <!-- Latest compiled and minified CSS -->
+<style>
+
+ .para{
+  top: 270px;
+  left: 135px;
+  font-size: 20px;
+  font-weight: 10;
+}
+ .topp {
+	
+  top: 150px;
+  left: 130px;
+  font-size: 90px;
+}
+
+
+
+
+</style>
            
 	</head>
 	<body>
   <ul class="topnav">
   <li><a class="active" href="logged.php">Home</a></li>
-  <li><a href="Admin.html">Create Poll</a></li>
+  <li><a href="Admin.php">Create Poll</a></li>
   <li><a href="view.php">View Answers</a></li>
   <li><a href="view_feedback.php">View Feedback</a></li>
   <li><a href="view_inq.php">View Inquiry</a></li>
+    <li><a href="viewinstantanswers.php">Quick Answers</a></li>
+	  <li><a href="InstantQ.php">Quick Question</a></li>
    <li><a href="Attachmentupload.php">Upload files</a></li>
-  <li class="right"><a href="logout.php">logout </a></li>
+  <li class="right"><a href="logout.php">Logout </a></li>
   </ul>
-   <a href="#" class="fa fa-facebook"></a>
-<a href="#" class="fa fa-twitter"></a>
-<a href="#" class="fa fa-google"></a>
-<p class="bottomleft"> Copyright 2019, Software Engineering Course, POLL ME. </p>
-<p class="topleft"> Poll Me</p>
-<p class="paragraph">Welcome to PollMe, you're now logged in, choose what you want to do from the upper bar</p>
+
+
+
+<p class="topp"> Poll Me</p>
+<p class="para">Welcome to PollMe, you're now logged in as Speaker, choose what you want to do from the upper bar</p>
+
       
   
 
