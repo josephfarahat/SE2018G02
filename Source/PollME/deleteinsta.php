9efc4a2b0898a487ca27ee500df5d53b69372f7e
josephfarahat <36537848+josephfarahat@users.noreply.github.com>
2019-02-14 21:42:10 +0200

    Add files via upload

---
+++ Source/PollME/deleteinsta.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<title>
+          Poll me 
+    
+    	</title>
+    	<link rel="stylesheet"
+    		       type = "text/css"
+    		       href="bootstrap.css"
+
+    		   />
+           <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+           <link rel="stylesheet"
+               type = "text/css"
+               href="index.css"
+               
+           />
+           
+	</head> 
+	<body>
+  <ul class="topnav">
+  <li><a class="active" href="index.php">Home</a></li>
+  <li><a href="answer_question.php"> Take Poll</a></li>
+  <li><a href="feedback2.html">Give feedback</a></li>
+  <li><a href="inquiry.html">Inquiry</a></li>
+  <li><a href="instantanswr.php">Quick question</a></li>
+  <li><a href="Attachmentdownload.php">Download files</a></li>
+  <li class="right"><a href="login.php">Are you Speaker?</a></li>
+   
+  </ul>
+  <a href="https://web.facebook.com/Poll-Me-2317018251900001/?eid=ARAZoGfUZYh_iFvHUp0t-jfPStyGkMNkQdWmr8mGarPCCitjEe9c6rMz8s6vj5naMvK4F22p40D3d4PK" class="fa fa-facebook"></a>
+
+ 
+<p class="bottomleft"> Copyright 2019, Software Engineering Course, POLL ME. </p>
+<p class="topleft"> Poll Me</p>
+<p class="paragraph">Welcome to PollMe, We offer  an efficient way of communication between a speaker and his audience.</p>
+      
+  
+
+	</body>
+</html>
